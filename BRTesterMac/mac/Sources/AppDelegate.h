3783395816328a2d445b726ee21f25ce90cf3735
Rauan Mayemir <rauan@mayemir.io>
2018-06-29 16:29:45 +0600

    Add XcodeGen dep, project spec, local tester app skeleton

---
+++ BRTesterMac/mac/Sources/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  Brisk
+//
+//  Created by Rauan Mayemir on 6/29/18.
+//  Copyright © 2018 Brisk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+// extern NSView *rootView;
+// extern dispatch_queue_t q;
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@end
---
+++ BRTesterMac/mac/Sources/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Brisk
+//
+//  Created by Rauan Mayemir on 6/29/18.
+//  Copyright © 2018 Brisk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+  return NSApplicationMain(argc, argv);
+}

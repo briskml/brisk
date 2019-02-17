5ffa7de37d38bfe7c255a8ff917aa635ae46a1ea
Ben <schinn.ben@gmail.com>
2019-02-17 00:11:56 -0700

    required methods in implementation

--- renderer-macos/lib/stubs/BriskTextInput.c
+++ renderer-macos/lib/stubs/BriskTextInput.c
@@ -1,15 +1,43 @@
-@interface BriskTextInput : NSTextInputContext <NSTextInputControl>
+#import "BriskStylableText.h"
+
+@interface BriskTextInput : NSTextInputContext <BriskStylableText>
 
 @end
 
 @implementation NSTextInputContext
-- (id)init {
-  self = [super init];
-  return self;
-}
-@property(nonatomic, assign) value _callback;
 
-- (void)setCallback:(value)action;
+- (instancetype)initWithClient:(id<NSTextInputClient>)client {
+  //designated initializer
+}
+
+- (BOOL)handleEvent:(NSEvent *)event {
+  //if system consumes event return YES
+}
+
+- (void)activate {
+  //invoked when receiver gets activated
+}
+
+- (void)deactivate {
+  //invoked when receiver gets deactivated
+}
+
+- (void)discardMarkedText {
+  //Notifies the system to discard the current conversion session. The client should clear its marked range when sending this message.
+}
+
+- (void)invalidateCharacterCoordinates {
+  //Notifies the text input system that information related to character positions, including the document's visual coordinates, text selection, and document contents, has been modified. Text engines implementing the NSTextInputClient protocol should send this message whenever any of these changes occur, and -[NSTextInputContext handleEvent:] will not otherwise be called. -handleEvent: serves as an implicit notification that any of these changes could have occurred.
+}
+
++ (nullable NSString *)localizedNameForInputSource:(NSTextInputSourceIdentifier)inputSourceIdentifier {
+  /**** Text Input source attributes ****/
+/* Returns the display name for inputSourceIdentifier.
+ */
+}
+
+@dynamic currentInputContext;
+
 @end
 
 NSTextInputContext *ml_BriskTextInput_make() {
@@ -19,11 +47,3 @@ NSTextInputContext *ml_BriskTextInput_make() {
   return input;
 }
 
-CAMLprim value ml_BriskTextInput_setCallback(BriskTextInput *input, value callback_v) {
-  CAMLparam1(callback_v);
-  value callback = callback_v;
-
-  [input setCallback:callback];
-
-  CAMLreturn(Val_unit);
-}
